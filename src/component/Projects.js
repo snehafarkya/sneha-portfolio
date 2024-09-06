ba2842921bc7c5892fe5b12ec34e7611681614b8
snehafarkya <snhafarkya@gmail.com>
2024-09-06 15:50:23 +0530

    mobile view animation updated

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -26,29 +26,29 @@ export default function Projects() {
           {/* Wondering about me? 👩‍💻 */}
           Work that I am <i>Proud</i> of 👇
         </Heading>
-      <article className="md:w-[1200px] md:mt-20 w-[380px] mx-auto  text-black box-border">
+      <article className="md:w-[1200px] md:mt-20 mt-20 w-[380px] mx-auto  text-black box-border">
 
         {work.slice(0,5).map((item, index) => {
           return (
 
             <figure
   key={index}
-  className="relative group mb-8 bg-gray-200 dark:bg-[#18224b] cursor-pointer rounded-lg w-full md:w-full flex items-center md:h-[600px] sticky top-28 overflow-hidden shadow-lg"
+  className="relative group mb-8 bg-gray-200 dark:bg-[#18224b] cursor-pointer rounded-lg w-full flex items-center md:h-[600px] sticky top-28 overflow-hidden shadow-lg"
 >
   <img
     src={item.image}
     alt={item.title}
-    className="w-[1100px] mx-auto group-hover:scale-105 transition-transform ease-in-out duration-500 rounded-xl border-4 border-black border-solid object-cover object-center"
+    className="md:w-[1100px] mx-auto group-hover:scale-105 transition-transform ease-in-out duration-500 md:rounded-xl rounded-lg border-4 border-black border-solid object-cover object-center"
   />
 
   <div className="absolute inset-0 bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col gap-4 justify-center items-center text-white">
-    <h1 className="text-4xl font-bold decoration-dashed underline underline-offset-4">
+    <h1 className="text-4xl hidden md:flex font-bold decoration-dashed underline underline-offset-4">
       {item.title}
     </h1>
-    <p className="text-center px-36 text-lg">
+    <p className="text-center hidden md:flex px-36 text-lg">
       {item.desc}
     </p>
-    <div className="flex gap-12">
+    <div className="hidden md:flex gap-12">
       <div className="block">
         <h3 className="text-lg">Role</h3>
         <p className="font-semibold text-[#ffa700]">Frontend Developer</p>
